29963682fd0bb83583e565758c6ecea4b3ae2988
MattMBenson <matt.benson245@gmail.com>
2023-01-22 14:54:27 -0500

    adding cursor functionality to work page

--- src/work.js
+++ src/work.js
@@ -2,9 +2,6 @@ import React, { useState, useRef, useEffect } from "react";
 import "./styles/fade.css";
 import Logo from "./svg-components/logo.js";
 import "./styles/work.css";
-import chevronRight from "./cursors/chevron-black.cur";
-import chevronLeft from "./cursors/chevron-white.cur";
-
 export default function Work() {
   const cursorWrapperRef = useRef(null);
   const [isActive, setIsActive] = useState(false);
@@ -26,29 +23,24 @@ export default function Work() {
       right: rect.right,
       bottom: rect.bottom,
     });
+    setMouseCoords({ x: event.clientX, y: event.clientY });
     if (event.clientX < coords.left + (coords.right - coords.left) / 2) {
-      event.target.style.cursor = `url(${chevronLeft}), auto`;
+      cursorWrapperRef.current.textContent = "PREVIOUS";
+      cursorWrapperRef.current.style.color = "#fff";
+      cursorWrapperRef.current.style.opacity = "1";
+      cursorWrapperRef.current.style.visibility = "visible";
     } else {
-      event.target.style.cursor = `url(${chevronRight}), auto`;
+      cursorWrapperRef.current.textContent = "NEXT";
+      cursorWrapperRef.current.style.color = "#000";
+      cursorWrapperRef.current.style.opacity = "1";
+      cursorWrapperRef.current.style.visibility = "visible";
     }
   };
 
-  const handleMouseMoveOutter = (event) => {
-    const rect = event.target.getBoundingClientRect();
-    setCoords({
-      top: rect.top,
-      left: rect.left,
-      right: rect.right,
-      bottom: rect.bottom,
-    });
-    setMouseCoords({ x: event.clientX, y: event.clientY });
-    if (event.clientX < coords.left + (coords.right - coords.left) / 2) {
-      cursorWrapperRef.current.visibility = "visible";
-      cursorWrapperRef.current.textContent = "Cursor on the left side";
-    } else {
-      cursorWrapperRef.current.style.visibility = "visible";
-      cursorWrapperRef.current.textContent = "Cursor on the right side";
-    }
+  const handleMouseLeave = (event) => {
+    cursorWrapperRef.current.style.visibility = "hidden";
+    cursorWrapperRef.current.textContent = "";
+    cursorWrapperRef.current.style.opacity = "0";
   };
 
   return (
@@ -58,21 +50,18 @@ export default function Work() {
     >
       <Logo />
 
+      <div className="work-container" id="0"></div>
       <div
-        className="work-container"
-        id="0"
+        className="data-cursor-tracker-x"
         onMouseMove={handleMouseMove}
-      ></div>
-      <div
-        className="data-cursor-tracker"
-        onMouseMove={handleMouseMoveOutter}
+        onMouseLeave={handleMouseLeave}
       ></div>
 
       <div className="light-side"></div>
       <div className="dark-side"></div>
       <p
         className="cursor-wrapper"
-        style={{ top: mouseCoords.y, left: mouseCoords.x }}
+        style={{ top: mouseCoords.y - 24, left: mouseCoords.x - 30 }}
         ref={cursorWrapperRef}
       ></p>
     </div>
